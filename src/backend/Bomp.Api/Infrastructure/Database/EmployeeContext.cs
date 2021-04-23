86479c6bb1be07dbff288acdd36150eddc84d5bf
Sander Meester <sander.meester@finaps.nl>
2021-04-23 10:20:50 +0200

    make it actually run haha

--- src/backend/Bomp.Api/Infrastructure/Database/EmployeeContext.cs
+++ src/backend/Bomp.Api/Infrastructure/Database/EmployeeContext.cs
@@ -6,5 +6,8 @@ namespace Bomp.Infrastructure.Database
   public class PokemonContext : DbContext
   {
     public DbSet<Pokemon> Pokemons { get; set; }
+    public PokemonContext(DbContextOptions<PokemonContext> options) : base(options)
+    {
+    }
   }
 }
